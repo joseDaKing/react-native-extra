30eb049914e662f41bfa809fb2d33360634afaab
yousef <yousefkhalil125@gmail.com>
2020-12-07 13:14:23 +0100

    refactored style-sheet, calculate packages to components package

---
+++ packages/components/src/style-sheet/index.ts
@@ -0,0 +1,17 @@
+import { 
+    useStyleFactory,
+    useStyle,
+    StyleProps,
+    StyleType
+}
+from "./use-style";
+
+import { compose } from "./compose";
+
+export {
+    StyleProps,
+    useStyleFactory,
+    useStyle,
+    StyleType,
+    compose
+};
