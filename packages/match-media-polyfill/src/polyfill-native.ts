eb0554a92c277f009c8505599093234fb6070dc9
yousef <yousefkhalil125@gmail.com>
2020-12-26 23:37:27 +0100

    cleaned unused packages

--- packages/match-media-polyfill/src/polyfill-native.ts
+++
@@ -1,9 +0,0 @@
-import { MediaQueryList } from "./media-query-list";
-
-const matchMedia = (query: string) => new MediaQueryList(query);
-
-if (typeof window !== "undefined") {
-
-    // @ts-ignore
-    global.matchMedia = matchMedia;
-}
