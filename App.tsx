1c192d21e8831af3c75e3941ac7531046c75a681
yousef <yousefkhalil125@gmail.com>
2020-12-27 00:16:19 +0100

    a

--- App.tsx
+++ App.tsx
@@ -2,25 +2,16 @@ import React from 'react';
 
 import { StyleSheet, Text, View } from 'react-native';
 
-import * as hooks from './packages/media-query/src';
-
-import "./packages/style-sheet/src/apply-styles";
-
 
 
 export default function App() {
-
+  
   return (
     <View 
     style={styles.container}>
-       
-      <hooks.OnMediaQuery
-      aspectRatio={[1, 2]}
-      maxWidth={1024}>
-        <Text>
-          Open up App.tsx to start working on your app!
-        </Text>
-      </hooks.OnMediaQuery>
+      <Text>
+        Open up App.tsx to start working on your app!
+      </Text>
     </View>
   );
 }
